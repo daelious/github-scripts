db842387fd4e29fcd89fb76306f989ac665708ec
Keith Meyer <meyer.keith@gmail.com>
2022-07-09 21:35:03 -0400

    Validation function to check for date and repo. Some OCD fixes too.

--- github-tag-clean.sh
+++ github-tag-clean.sh
@@ -4,8 +4,32 @@ REPO=""
 DRY_RUN=false
 LOCAL_ONLY=false
 
+trap ctrl_c INT
+function ctrl_c() {
+  echo "The execution of the script was aborted due to user entering Ctrl-c"
+  exit -1
+}
+
 function usage() {
   echo "github-tag-clean.sh -d/--date YYYY-MM-DD -r/--repo <repo_uri> {-D/--dry-run} {-l/--local-only}"
+}
+
+function validate() { 
+  NOW=$(date +%Y-%m-%d)
+  URL_REGEX="(https|git):\/\/github.com\/[a-zA-Z0-9]+\/[a-zA-Z0-9]+(\/|.git)?"
+  if [ -z $DATE ]; then
+    echo "Date is required."
+  elif [[ $DATE > $NOW ]]; then
+    echo "Date cannot be in the future"
+    exit -1    
+  fi
+
+  if [ -z $REPO ]; then
+    echo "Repository must be specified."
+  elif [[ ! $REPO =~ $URL_REGEX ]]; then
+    echo "Repository is invalid."
+    exit -1
+  fi
 }
 
 short_args=d:r:Dlh
